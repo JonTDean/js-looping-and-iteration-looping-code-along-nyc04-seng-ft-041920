78e1e647e7981eb90e040ea82abd9f3943bd2357
JonTDean <JonathanTomDean@Gmail.com>
2020-06-18 17:37:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+    // Writes the text to be stored in new Array
+function writeCards(cards){
+    let writtenCards = []                                               //  Creates empty Array
+
+    for(let i = 0; i < cards.length ; i++){                             //  Iterates through Given Array
+        //  Pushes data into new array
+        writtenCards.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);   
+    }
+
+    return writtenCards;
+};
+
+function countDown(num){
+    while(num != 0){
+        console.log(num) ;
+        num-- ;
+    }
+    console.log(num) ;
+}
